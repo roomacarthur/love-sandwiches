1159b4fd85cc90a27b320d866a10d21139f98aa8
Roomacarthur <roomacarthur@gmail.com>
2021-06-11 17:36:09 +0100

    removed old worksheet update functions after refactoring

--- run.py
+++ run.py
@@ -50,24 +50,6 @@ def validate_data(values):
     
     return True
 
-# def update_sales_worksheet(data):
-#     """
-#     Update sales worksheet, add new row with the list data provided. 
-#     """
-#     print("Updating sales worksheet...\n")
-#     sales_worksheet = SHEET.worksheet("sales")
-#     sales_worksheet.append_row(data)
-#     print("Sales worksheet updated successfully.\n")
-
-# def update_surplus_worksheet(data):
-#     """
-#     Add surplus data retrieved from calculate_surplus_data() to the surplus worksheet.
-#     """
-#     print("Updating surplus worksheet...\n")
-#     surplus_worksheet = SHEET.worksheet("surplus")
-#     surplus_worksheet.append_row(data)
-#     print("Updates surplus worksheet successfully.\n")
-
 def update_worksheet(data, worksheet):
     """
     Update sales and surplus worksheets, append new row with the list data provided.
